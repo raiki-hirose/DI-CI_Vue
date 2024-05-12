132115082e85a8cef3662c9a4c46b264d156c218
raiki-hirose <raiki.hirose@asia-quest.jp>
2024-05-12 14:37:00 +0900

    環境構築完了＋Route設定・メインページview作成

---
+++ vue3-tutorial/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink :to="{ name: 'home'}">Home</RouterLink>
+        <RouterLink :to="{ name: 'create' }">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <main>
+
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue3-tutorial/src/views/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  
+</template>
+
+<stype scoped>
+
+</stype>
